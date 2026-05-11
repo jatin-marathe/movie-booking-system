820120e924d2123a2baaa4de05f9d464303d09fd
Jatin Marathe <jatinmarathe@gmail.com>
2026-05-11 16:49:41 +0530

    change in server.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api`,
+    baseURL: `${baseURL}/api`,
     headers: { 'Content-Type': 'application/json' },
 });
 
