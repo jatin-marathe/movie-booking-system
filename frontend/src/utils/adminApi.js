b5a7546831257840d3708ed2bbc31766ca0a1e0c
Jatin Marathe <jatinmarathe@gmail.com>
2026-05-11 16:02:21 +0530

    updates

--- frontend/src/utils/adminApi.js
+++ frontend/src/utils/adminApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const adminApi = axios.create({
-    baseURL: '/api/admin',
+    baseURL: `${import.meta.env.VITE_API_URL}/api/admin`,
     headers: { 'Content-Type': 'application/json' },
 });
 
